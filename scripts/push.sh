794b413b2141198ef6361c86da4ceac4bfc32bf2
Re-Wi <RejoiceWindow@yeah.net>
2023-02-22 10:07:54 +0800

    V0.1.3 : 更新donate；修改提交、发布脚本

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,5 +2,5 @@ VERSION=`node -p -e "require('./package.json').version"`
 COMMIT_M=`node -p -e "require('./package.json').commit_m"`
 
 git add --all
-git commit -m "V${VERSION}: ${COMMIT_M}"
+git commit -m "V${VERSION} : ${COMMIT_M}"
 git push
